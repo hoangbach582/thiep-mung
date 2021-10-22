0a9fd13bd6b87fda259257ac875eb9509aaa898c
hoangbach582 <92920348+hoangbach582@users.noreply.github.com>
2021-10-22 18:59:58 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const nameGirl = '20-10 vui vẻ bạn nhó!!';
-const giftUrl = 'http://nodemy.vn';
+const nameGirl = 'bét phờ ren';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100049234566172';
 const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc bạn của tôi 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn Chang trong hôm nay và cả những ngày sau';
+const titleCard = 'Tặng bạn Trangg con bác Hồng nhó!!';
+const contentCard = 'Chúc bạn Chang luôn luôn mạnh khỏe và ngày càng xinh đẹp, đặc biệt là sẽ đạt được điểm cao trong kì thi sắp tới.';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = 'hot-girl1.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
